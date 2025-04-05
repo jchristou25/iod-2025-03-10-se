fd08dad76a6bc555edb2137424335a6986b07e12
Julianne Christou <juliechristou@gmail.com>
2025-04-05 15:41:34 -0400

    Module1 exercises - additional work

---
+++ Module1/Exercise6/Exercise6.js
@@ -0,0 +1,7 @@
+const book1 = { 
+    title: "Pride and Prejudice",
+    description: "A love story between an equally proud and prejudiced Elizabeth Bennett and Mr. Darcy",
+    pages: "259",
+    author: "Jane Austen",
+
+}
